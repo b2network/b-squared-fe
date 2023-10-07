ee56c4dda78fbbca1d8a7294a585ec0ef8b82b29
lance10030 <lance10030@163.com>
2023-10-07 09:50:04 +0800

    feat: css change

--- components/Home/ValueMission.tsx
+++ components/Home/ValueMission.tsx
@@ -12,7 +12,7 @@ const ValueMission = () => {
   const MissionBox = styled(Box)(({ }) => ({
     display: 'flex',
     alignItems: 'center',
-    padding: isMobile ? '10px' : '54px 80px',
+
     background: "#2A2A2A",
   }));
   return (
@@ -20,23 +20,30 @@ const ValueMission = () => {
       p: { xs: '20px', md: '80px 100px' },
       color: 'white',
       background: 'url("assets/mission_bg.svg") no-repeat center center',
-      backgroundSize: 'cover'
+      backgroundSize: 'cover',
+      overflow: 'hidden'
     }}>
       <Box sx={{ display: 'flex', flexDirection: isMobile ? 'column' : 'row', alignItems: 'flex-start', mb: '60px' }}>
-        <Typography fontSize={isMobile ? 25 : 50} fontWeight={600} mb={isMobile ? '20px' : 0} mr={{ xs: 0, sm: '55px', md: '125px' }}  data-aos='fade-right'>Value</Typography>
-        <Typography fontSize={isMobile ? 18 : 28} data-aos='fade-left'>
+        <Box fontSize={{ lg: '50px', md: '40px', sm: '30px', xs: '25px' }} fontWeight={600} mb={isMobile ? '20px' : 0} width={{ xs: 'auto', sm: '160px', lg: '210px' }} mr={{ xs: 0, sm: '30px', md: '100px' }} data-aos='fade-right'>Value</Box>
+        <Typography fontSize={isMobile ? 18 : 28} data-aos='fade-left' flex={1}>
           Bitcoin, as the primary crypto asset, can evolve beyond a payment network.
           BTC-related assets require an ecosystem where DeFi, NFT, SocialFi, and other applications can thrive and flourish.
         </Typography>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'flex-start', flexDirection: isMobile ? 'column' : 'row' }}>
-        <Typography fontSize={isMobile ? 25 : 50} fontWeight={600} mb={isMobile ? '20px' : 0} mr={{ xs: 0, sm: '30px', md: '100px' }} data-aos='fade-right'>Mission</Typography>
+        <Box fontSize={{ lg: '50px', md: '40px', sm: '30px', xs: '25px' }} fontWeight={600} mb={isMobile ? '20px' : 0} mr={{ xs: 0, sm: '30px', md: '100px' }} width={{ xs: 'auto', sm: '160px', lg: '210px' }} data-aos='fade-right'>Mission</Box>
         <Box flex={1}>
-          <MissionBox sx={{ mb: '20px' }} data-aos='fade-left'>
+          <MissionBox
+            sx={{
+              mb: '20px',
+              p: { xs: '10px', md: '20px', lg: '54px 80px' },
+            }}
+            data-aos='fade-left'
+          >
             <CheckCircleOutlineIcon sx={{ fontSize: isMobile ? '20px' : '48px', color: 'rgba(255, 184, 82, 1)' }} />
             <Typography ml={isMobile ? '10px' : '20px'} fontSize={isMobile ? '18px' : '28px'}>Build the most practical Bitcoin layer-2 network</Typography>
           </MissionBox>
-          <MissionBox data-aos='fade-left'>
+          <MissionBox sx={{ p: { xs: '10px', md: '20px', lg: '54px 80px' }, }} data-aos='fade-left'>
             <CheckCircleOutlineIcon sx={{ fontSize: isMobile ? '20px' : '48px', color: 'rgba(255, 184, 82, 1)' }} />
             <Typography ml={isMobile ? '10px' : '20px'} fontSize={isMobile ? '18px' : '28px'}>Bring builder culture back to the Bitcoin ecosystem</Typography>
           </MissionBox>
