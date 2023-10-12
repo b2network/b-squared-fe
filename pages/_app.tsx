bf658296d3e6d71898fcdc9004ccf814aad4d757
lance10030 <lance10030@163.com>
2023-10-12 19:29:09 +0800

    feat: title change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,7 +40,7 @@ const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = (
         {getLayout(
           <>
             <Head>
-              <title></title>
+              <title>B² Network</title>
               <meta
                 name='viewport'
                 content='width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover'
