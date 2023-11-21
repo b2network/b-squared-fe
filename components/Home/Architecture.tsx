3a17459353122dcd10f427f41aa1d9b1a4b1ce58
lance10030 <lance10030@163.com>
2023-11-21 11:16:19 +0800

    feat: disable image select

--- components/Home/Architecture.tsx
+++ components/Home/Architecture.tsx
@@ -22,7 +22,7 @@ const Architecture = () => {
       }}>
         <Box sx={{ background: 'white',borderRadius: '8px',width:'100%',p:'30px' }}>
           <Typography textTransform='uppercase' data-aos='fade-down' fontFamily='Hanson' textAlign={'left'} mb={'30px'} color={'black'} fontSize={isMobile ? '20px' : '30px'} fontWeight={700}>Architecture of B²</Typography>
-            <img className='hvr-grow' data-aos='fade-up' src="/assets/architecture.png" style={{ width:  '100%', display: 'block', margin: 'auto' }} alt="architeture" />
+            <img className='hvr-grow' data-aos='fade-up' src="/assets/architecture.png" style={{ width:  '100%', display: 'block', margin: 'auto',pointerEvents:'none',userSelect:'none' }} alt="architeture" />
         </Box>
       </Box>
     </Box>
