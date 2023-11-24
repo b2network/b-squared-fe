f74ddce1b128a675cb79ecc85c04e6bf594079bf
lance10030 <lance10030@163.com>
2023-11-24 21:43:41 +0800

    fix: build err

--- hooks/bridgeContract.ts
+++ hooks/bridgeContract.ts
@@ -2,13 +2,13 @@ import { getContract } from "utils/contract";
 
 import { Signer } from "ethers";
 import { BridgeContract } from "constant";
-// import BridgeAbi from "constant/abi/Ibridge.json";
+import BridgeAbi from "constant/abi/bridge.json";
 
 export const getBrigeContract = (signer?: Signer) => {
   if(!signer)return
   return getContract(
     BridgeContract,
-    ['function withdraw(string btc_address) payable'],
+    BridgeAbi,
     signer
   )
 };
