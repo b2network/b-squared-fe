a35bcad9ff18fdf5d9f0e3ab275924486b3be0b3
lance10030 <lance10030@163.com>
2023-11-30 18:10:42 +0800

    refactor: dependency

---
+++ src/components/Home/Architecture.tsx
@@ -0,0 +1,33 @@
+import useIsMobile from '@/hooks/useIsMobile';
+import {
+  Box,
+  Typography,
+} from '@mui/material';
+// import useIsMobile from '@/hooks/useIsMobile';
+
+const Architecture = () => {
+  const isMobile = useIsMobile();
+
+  return (
+    <Box sx={{
+      background: 'white',
+    }}>
+      <Box sx={{
+        maxWidth: '1290px',
+        margin: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        background: '#000',
+        p: '1px'
+      }}>
+        <Box sx={{ background: 'white',borderRadius: '8px',width:'100%',p:'30px' }}>
+          <Typography textTransform='uppercase' data-aos='fade-down' fontFamily='Hanson' textAlign={'left'} mb={'30px'} color={'black'} fontSize={isMobile ? '20px' : '30px'} fontWeight={700}>Architecture of B²</Typography>
+            <img className='hvr-grow' data-aos='fade-up' src="/assets/architecture.png" style={{ width:  '100%', display: 'block', margin: 'auto',pointerEvents:'none',userSelect:'none' }} alt="architeture" />
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default Architecture;
