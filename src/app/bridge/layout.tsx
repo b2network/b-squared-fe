96fb1fd322fd614abb5e6a29f804eaf3e090f857
lance10030 <lance10030@163.com>
2024-01-01 09:04:34 +0800

    feat: auto connect

--- src/app/bridge/layout.tsx
+++ src/app/bridge/layout.tsx
@@ -1,7 +1,23 @@
+'use client'
 import Layout from "@/components/Layout"
-import { ReactNode } from "react"
+import { StoreWalletKey } from "@/utils"
+import { BtcConnectorName, useBtc } from "@/wallets/btcWallet"
+import { ReactNode, useEffect } from "react"
 
 const LayoutWrapper: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const { connect } = useBtc()
+
+  const init = () => {
+    const connectName = localStorage.getItem(StoreWalletKey);
+    if (connectName) {
+      connect(connectName as BtcConnectorName)
+    }
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
+
   return (
     <Layout showFooter={false}>{children}</Layout>
   )
