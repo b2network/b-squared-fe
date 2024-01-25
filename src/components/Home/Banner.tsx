abfe8384ff6d9d59cb776429a6db13ae327461e3
lance10030 <lance10030@163.com>
2024-01-25 11:47:31 +0800

    feat: style change

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -6,19 +6,20 @@ import { BuzzUrl } from "@/utils";
 const Banner = () => {
   return (
     <Box
-      onClick={() => { 
+      onClick={() => {
         window.open(BuzzUrl)
       }}
       sx={{
-      background: 'url("/assets/banner_buzz.png") no-repeat center center',
-      height: '120px',
-      backgroundSize: 'cover',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      gap: '14px',
-      textTransform: 'uppercase'
-    }}>
+        background: 'url("/assets/banner_buzz.png") no-repeat center center',
+        height: '120px',
+        backgroundSize: 'cover',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '14px',
+        textTransform: 'uppercase',
+        cursor: 'pointer'
+      }}>
       <Box sx={{ color: 'white', fontSize: '26px', fontWeight: 900 }}>Deposit Token to Earn Airdrop</Box>
       <EastIcon sx={{ color: 'white' }} />
     </Box>
