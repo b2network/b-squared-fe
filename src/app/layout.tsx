c08deab0fae7d99025cb662a317a311c50e6785b
lance10030 <lance10030@163.com>
2024-01-26 20:11:55 +0800

    feat: add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import '@/styles/global.scss'
 import { Providers } from './providers'
 import { AOSInit } from './aos'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AOSInit />
+      <GoogleTagManager gtmId="G-6C7Q53GBMN" />
       <body >
         <Providers>{children}</Providers>
       </body>
