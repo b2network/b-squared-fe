65bf0aa962c493275bdf572e05d79582ca908913
zxp1232 <zyy9913@outlook.com>
2024-02-05 11:37:32 +0800

    feat(develop-zyy): change banner img and kucoin icon

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,28 +1,58 @@
 import { Box } from "@mui/material"
 import EastIcon from '@mui/icons-material/East';
 import { BuzzUrl } from "@/utils";
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const Banner = () => {
   return (
-    <Box
-      onClick={() => {
-        window.open(BuzzUrl)
-      }}
-      sx={{
-        background: 'url("/assets/banner_buzz.png") no-repeat center center',
-        height: '120px',
-        backgroundSize: 'cover',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '14px',
-        textTransform: 'uppercase',
-        cursor: 'pointer'
-      }}>
-      <Box sx={{ color: 'white', fontSize: '26px', fontWeight: 900 }}>Join B² Buzz</Box>
-      <EastIcon sx={{ color: 'white' }} />
-    </Box>
+    <Swiper
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <Box
+          onClick={() => {
+            window.open(BuzzUrl)
+          }}
+          sx={{
+            background: 'url("/assets/banner_buzz.png") no-repeat center center',
+            height: '120px',
+            backgroundSize: 'cover',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '14px',
+            textTransform: 'uppercase',
+            cursor: 'pointer'
+          }}>
+          <Box sx={{ color: 'white', fontSize: '26px', fontWeight: 900 }}>Join B² Buzz</Box>
+          <EastIcon sx={{ color: 'white' }} />
+        </Box>
+      </SwiperSlide>
+      {/* <SwiperSlide>
+        <Box
+          onClick={() => {
+            window.open(BuzzUrl)
+          }}
+          sx={{
+            background: 'url("/assets/banner_buzz.png") no-repeat center center',
+            height: '120px',
+            backgroundSize: 'cover',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '14px',
+            textTransform: 'uppercase',
+            cursor: 'pointer'
+          }}>
+          <Box sx={{ color: 'white', fontSize: '26px', fontWeight: 900 }}>Join B² Buzz</Box>
+          <EastIcon sx={{ color: 'white' }} />
+        </Box>
+      </SwiperSlide> */}
+    </Swiper>
+
   )
 }
 
--- src/components/Home/Invest.tsx
+++ src/components/Home/Invest.tsx
@@ -6,7 +6,7 @@ const INVESTLIST = [
   '/assets/invest/icon_okx.svg',
   '/assets/invest/icon_abcde.svg',
   '/assets/invest/icon_idg.svg',
-  '/assets/invest/icon_kucoin.svg',
+  '/assets/invest/icon_kucoin.png',
   '/assets/invest/icon_water.svg',
   '/assets/invest/icon_satoshi.svg',
   '/assets/invest/icon_antalpha.svg',
@@ -23,7 +23,7 @@ const Invest = () => {
       <Box data-aos='fade-up'  sx={{ fontSize: '30px', fontWeight: 700, fontFamily: 'Hanson', color: primaryColor, mb: '30px' }}>Invested By</Box>
       <Grid container spacing={4} justifyContent={'center'}>
         {
-          INVESTLIST.map((v, i) => <Grid data-aos='fade-up' item key={i}> <img src={v} alt="icon" /></Grid>)
+          INVESTLIST.map((v, i) => <Grid data-aos='fade-up' item key={i}> <img style={{width: "220px", height: "61px"}} src={v} alt="icon" /></Grid>)
         }
       </Grid>
     </Box>
